871a697953c07f4b9974a0b4e17e697b425f1f51
benie254 <davinci.monalissa@gmail.com>
2023-06-20 09:46:30 +0300

    create user & delivery classes

---
+++ Deliver/src/app/classes/delivery/delivery.ts
@@ -0,0 +1,10 @@
+export class Delivery {
+    id?:number = 0;
+    amount:number = 0;
+    price_pl:number = 0;
+    delivered:string = '';
+    delivered_by:string = '';
+    received_from:string = '';
+    mobile:number = 0;
+    location:string = '';
+}
---
+++ Deliver/src/app/classes/user/user.ts
@@ -0,0 +1,9 @@
+export class User {
+    id?:number = 0;
+    token?: string = '';
+    refreshToken?: string = '';
+    username:string = '';
+    email:string = '';
+    password:string = '';
+    password2: string = '';
+}
