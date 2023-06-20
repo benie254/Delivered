e7983b26b20215b806e1c148f40ca8d92e02cd7a
benie254 <davinci.monalissa@gmail.com>
2023-06-20 10:16:53 +0300

    nest change pass form in the change pass component

--- Deliver/src/app/auth/components/change-pass/change-pass.component.ts
+++ Deliver/src/app/auth/components/change-pass/change-pass.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-change-pass',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-pass.component.css']
 })
 export class ChangePassComponent implements OnInit {
+  authenticated: boolean = false;
 
-  constructor() { }
+  constructor(
+    private authService:AuthService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    if (this.authService.currentUserValue) {
+      this.authenticated = true;
+    } else {
+      this.authenticated = false;
+      this.router.navigate(['/auth'])
+    }
   }
+  
 
 }
