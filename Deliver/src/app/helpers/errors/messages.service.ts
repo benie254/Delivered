acdc4b98f947a8a941898769175cc2ae72a6b8d3
benie254 <davinci.monalissa@gmail.com>
2023-06-20 09:52:56 +0300

    create add and clear functions in message service

--- Deliver/src/app/helpers/errors/messages.service.ts
+++ Deliver/src/app/helpers/errors/messages.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'any'
 })
 export class MessagesService {
+  messages: string[] = [];
 
-  constructor() { }
+  add(message: string){
+    this.messages.push(message)
+  }
+  clear(){
+    this.messages = [];
+  }
 }
