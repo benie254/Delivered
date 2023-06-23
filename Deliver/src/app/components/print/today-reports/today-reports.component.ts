3d340a2cda6763803017b7cbcc625c02b337f01c
benie254 <davinci.monalissa@gmail.com>
2023-06-23 09:08:22 +0300

    add today reports component logic

--- Deliver/src/app/components/print/today-reports/today-reports.component.ts
+++ Deliver/src/app/components/print/today-reports/today-reports.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Notiflix from 'notiflix';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { DeliveryService } from 'src/app/services/delivery/delivery.service';
 
 @Component({
   selector: 'app-today-reports',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./today-reports.component.css']
 })
 export class TodayReportsComponent implements OnInit {
+  todayReps: any;
+  today = new Date();
+  authenticated: boolean = false;
 
-  constructor() { }
+  constructor(
+    private delService: DeliveryService,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    if(this.authService.currentUserValue){
+      this.authenticated = true; 
+    } else {
+      this.authenticated = false; 
+      this.authService.logout();
+      Notiflix.Report.failure(
+        "Unauthorized!",
+        "Sorry, you do not have permissions to view this page.",
+        "Too bad"
+      )
+      this.router.navigate(['/'])
+      setTimeout(() => {
+        location.reload();
+      },5000)
+    }
+    this.todayReports();
+  }
+  todayReports(){
+    Notiflix.Loading.dots('processing...')
+    this.delService.getTodayReports().subscribe({
+      next: (res) => {
+        Notiflix.Loading.remove();
+        this.todayReps = res;
+      }
+    })
+  }
+  print(){
+    window.print();
   }
 
 }
