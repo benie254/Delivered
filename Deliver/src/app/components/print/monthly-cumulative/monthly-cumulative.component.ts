c34129664617e9b6d7b6ae305c7073a3d01bbeb4
benie254 <davinci.monalissa@gmail.com>
2023-06-23 09:07:14 +0300

    add monthly cumulative component's logic

--- Deliver/src/app/components/print/monthly-cumulative/monthly-cumulative.component.ts
+++ Deliver/src/app/components/print/monthly-cumulative/monthly-cumulative.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as Notiflix from 'notiflix';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { DeliveryService } from 'src/app/services/delivery/delivery.service';
 
 @Component({
   selector: 'app-monthly-cumulative',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monthly-cumulative.component.css']
 })
 export class MonthlyCumulativeComponent implements OnInit {
+  monthlyCumul: any;
+  month = new Date().getMonth();
+  year = new Date().getFullYear();
+  authenticated: boolean = false;
 
-  constructor() { }
+  constructor(
+    private delService: DeliveryService,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    if(this.authService.currentUserValue){
+      this.authenticated = true; 
+    } else {
+      this.authenticated = false; 
+      this.authService.logout();
+      Notiflix.Report.failure(
+        "Unauthorized!",
+        "Sorry, you do not have permissions to view this page.",
+        "Too bad"
+      )
+      this.router.navigate(['/'])
+      setTimeout(() => {
+        location.reload();
+      },5000)
+    }
+    this.monthlyCumulative();
+  }
+  monthlyCumulative(){
+    Notiflix.Loading.dots('processing...')
+    this.delService.getMonthlyCumulative().subscribe({
+      next: (res) => {
+        Notiflix.Loading.remove();
+        this.monthlyCumul = res;
+      }
+    })
+  }
+  print(){
+    window.print();
   }
 
 }
